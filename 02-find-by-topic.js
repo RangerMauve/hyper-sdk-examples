bd8a37aab3306b2a7598ffbcb0311b8eaf82d5e4
Mauve Signweaver <contact@mauve.moe>
2025-05-16 19:40:08 -0400

    Working lesson 3

--- 02-find-by-topic.js
+++ 02-find-by-topic.js
@@ -5,16 +5,18 @@ const sdk = await create({
   storage: false
 })
 
+console.log(sdk.publicKey.toString('hex'))
+
 process.on('SIGTERM', () => {
   console.log('Terminating')
   sdk.close()
 })
 
 sdk.on('peer-add', (peerInfo) => {
-  console.log('connected', peerInfo.publicKey)
+  console.log('connected', peerInfo.publicKey.toString('hex'))
 })
 sdk.on('peer-remove', (peerInfo) => {
-  console.log('disconnected', peerInfo.publicKey)
+  console.log('disconnected', peerInfo.publicKey.toString('hex'))
 })
 
 console.log('Joining')
